e9a197de453d6d86168919c3fbc8fc77ebc8b887
Orpheus421 <JRPollock98@gmail.com>
2017-03-26 10:39:37 -0400

    Lots of changes because I was too lazy to upload

--- CD_classes_and_functions.py
+++ CD_classes_and_functions.py
@@ -1,5 +1,4 @@
 import pygame as pg
-import random as rdm
 
 # Scaling screen function
 def scale_and_new_coords(map):
@@ -16,6 +15,22 @@ def scale_and_new_coords(map):
 	scaled_map.convert()
 	return (scaled_map, x, y, screen_w, screen_h)
 	
+	
+def in_range(x, y, w, h, button_position, button_size, cursor_position):
+	return (cursor_position[0]>(button_position[0]*w+x) and cursor_position[0]<((button_position[0]+button_size[0])*w+x) 
+	and cursor_position[1]>(button_position[1]*h+y) and cursor_position[1]<((button_position[1]+button_size[1])*h+y))
+
+#Stats class
+class Stats:
+	def __init__(self, health=50, gold=25):
+		self.hp=health
+		self.g=gold
+		
+	def display(self, my_font):
+		health=my_font.render("Health: "+str(self.hp), True, (15, 192, 7))
+		gold=my_font.render("Gold: "+str(self.g), True, (15, 192, 7))
+		return (health, gold)
+	
 #Enemy class
 class Enemy:
 	def __init__(self, position, color):
@@ -24,17 +39,17 @@ class Enemy:
 	
 	def move(self):
 		if self.y < .195:
-			self.y += .001
+			self.y += .0016
 		elif self.x < 0.61 and self.y < .4:
-			self.x += .001
+			self.x += .0009
 		elif self.y < .475:
-			self.y += .001
+			self.y += .0016
 		elif self.x > .115 and self.y < .7:
-			self.x -= .001
+			self.x -= .0009
 		elif self.y < .75 and self.x < 0.15:
-			self.y += .001
+			self.y += .0016
 		elif self.x < 0.761:
-			self.x += .001
+			self.x += .0009
 		else:
 			return 1
 		return 0
@@ -50,10 +65,13 @@ class Enemy:
 class Slots:
 	def __init__ (self, position1, position2, size1, size2):
 		self.empty = Button(position1, size1, 1, "empty")
-		self.buy = Button(position2, size2, 1, "buy")
+		self.buy = Button(position2, size2, 0, "buy")
 		self.tower1 = Button(position1, size1, 0, "tower1")
 		self.upgrade = Button(position2, size2, 0, "upgrade")
 		self.tower2 = Button(position1, size1, 0, "tower2")
+		self.cooldown = 0
+		self.firing_time = 0
+		self.enemy=0
 		
 	def display(self, screen, x, y, w, h):
 		if self.empty.visible:
@@ -66,6 +84,29 @@ class Slots:
 				self.upgrade.display(screen, x, y, w, h)
 		else:
 			self.tower2.display(screen, x, y, w, h)
+			
+	def tower_is_pressed(self, x, y, w, h, cursor_position):
+		return in_range(x, y, w, h, (self.empty.x, self.empty.y), 
+		(self.empty.h, self.empty.w), cursor_position)
+	
+	def purchase(self, player):
+		if self.empty.visible and player.g>15:
+			self.empty.visible=0
+			self.tower1.visible=1
+			player.g-=15
+		elif self.tower1.visible and player.g>30:
+			self.tower1.visible=0
+			self.tower2.visible=1
+			player.g-=30
+
+	def enter_shop(self):
+		if self.tower2.visible:
+			return False
+		elif self.tower1.visible:
+			self.upgrade.visible=1
+		else:
+			self.buy.visible=1
+		return True
 
 class Button:
 	def __init__ (self,position,size,visible,type):
--- CYB3R-D3F3NS3.py
+++ CYB3R-D3F3NS3.py
@@ -1,21 +1,31 @@
 import pygame as pg
 from CD_classes_and_functions import *
 import random as rdm
+from numpy import sqrt
 
-# Official Shade of Green: "0fc007"
+# Green is a very creative color: "0fc007"
 
 # Pygame initiation
 pg.init()
+pg.font.init()
 screen = pg.display.set_mode((1200, 700), pg.RESIZABLE)
 
 # Logo and Title
-pg.display.set_caption("CYB3R-D3F3NS3")
 logo=pg.image.load("C-D_logo.png")
 pg.display.set_icon(logo)
+pg.display.set_caption("CYB3R-D3F3NS3")
 
 # Initialization Stuff
 done = False
+inProgress = True
 clock = pg.time.Clock()
+
+title_font=pg.font.SysFont('couriernew', 36, bold=True)
+title_font.set_underline(True)
+title= title_font.render("CYB3R-D3F3NS3", True, (15, 192, 7))
+price = 0
+
+my_font=pg.font.SysFont('couriernew', 24)
 
 map=pg.image.load("better-map.png")
 scaled_map, x, y, w, h =scale_and_new_coords(map)
@@ -23,33 +33,111 @@ enemy_size=int(0.06*h)
 screen.fill((0, 0, 0))
 
 enemies=[]
-cooldown=0
+spawnrate=150
+
+player=Stats()
+health, gold = player.display(my_font)
 
 all_slots=[]
-slot_data=[(0.26, 0.02), ( 0.47, 0.02), (0.21, 0.31), (0.47,0.31), (0.3, 0.59), (0.61, 0.59), (0.25, 0.86), (0.56, 0.86)]
+slot_data=[(0.26, 0.02), ( 0.47, 0.02), (0.21, 0.31), (0.47,0.31), 
+(0.3, 0.59), (0.61, 0.59), (0.25, 0.86), (0.56, 0.86)]
+purchase_position=(0.77, 0.3)
+purchase_size=(0.2, 0.08)
+visible_purchase=-1
 for data in slot_data:
-	slot=Slots(data, (0.77, 0.3), (0.1, 0.1), (0.2, 0.08))
+	slot=Slots(data, purchase_position, (0.1, 0.1), purchase_size)
 	all_slots.append(slot)
 
+	
 # Heart of the game
 while not done:
 	for event in pg.event.get():
 		if event.type == pg.QUIT:
 			done = True
-#		How to detect if a key is pressed
-#		if event.type == pg.KEYDOWN and event.key == pg.K_SPACE:
+		if event.type == pg.MOUSEBUTTONDOWN and (pg.mouse.get_pressed())[0] and inProgress:
+			cursor_position=pg.mouse.get_pos()
+			what_was_pressed=0
+			if (in_range(x, y, w, h, purchase_position, purchase_size, cursor_position) 
+			and (visible_purchase !=-1)):
+				what_was_pressed=1
+			else:
+				count=0
+				for slot in all_slots:
+					success=slot.tower_is_pressed(x, y, w, h, cursor_position)
+					if success:
+						what_was_pressed=2
+						break
+					count+=1
+			for slot in all_slots:
+				slot.buy.visible=0
+				slot.upgrade.visible=0
+				price=0
+			if what_was_pressed==1:
+				(all_slots[visible_purchase]).purchase(player)
+				player.display(my_font)
+			elif what_was_pressed==2:
+				if all_slots[count].enter_shop():
+					visible_purchase=count
+					price=my_font.render("Costs "+("30" if all_slots[count].tower1.visible else "15")+" gold.", True, (15, 192, 7))
+			
 		if event.type == pg.VIDEORESIZE:
 			new_size= event.size
 			screen=pg.display.set_mode((new_size), pg.RESIZABLE)
 			scaled_map, x, y, w, h =scale_and_new_coords(map)
 			enemy_size=int(0.06*h)
 	
+	if not inProgress:
+		continue
+	
 	screen.blit(scaled_map, (x,y))
+	screen.blit(health, ((0.77*w)+x,(0.15*h)+y))
+	screen.blit(gold, ((0.77*w)+x,(0.225*h)+y))
+	screen.blit(title, ((0.705*w)+x,(0.05*h)+y))
+	if price:
+		screen.blit(price, ((0.77*w)+x,(0.42*h)+y))
 	
-	cooldown -=1
 	
-	num=int(rdm.random()*100)
-	if num<2 and cooldown<0:
+	for slot in all_slots:
+		if not slot.empty.visible:
+			if not slot.cooldown:
+				#Math to find closest mob
+				if enemies:
+					distance=[]
+					for manny in enemies:
+						distance.append(sqrt(((slot.empty.x-manny.x)*(9/16))**2+(slot.empty.y-manny.y)**2))
+					if min(distance) < (0.4 if slot.tower2.visible else 0.25):
+						slot.enemy=enemies[distance.index(min(distance))]
+					slot.firing_time = 30
+					width=5
+				if slot.tower1.visible:
+					slot.cooldown=150
+				else:
+					slot.cooldown=90
+			elif slot.firing_time>1:
+				if slot.enemy:
+					pg.draw.line(screen, (  255,   255, 255)
+					, ((slot.empty.x+0.03)*w+x, (slot.empty.y+0.05)*h+y)
+					, ((slot.enemy.x+0.03)*w+x, (slot.enemy.y+0.03)*h+y), width) 
+				slot.firing_time-=1
+				if width<(15 if slot.tower2.visible else 5):
+					width+=1
+				else:
+					width=(6 if slot.tower2.visible else 1)
+			elif slot.firing_time==1:
+				slot.firing_time =0
+				if slot.enemy in enemies:
+					enemies.remove(slot.enemy)
+				slot.enemy=0
+				player.g+=1
+				health, gold = player.display(my_font)
+				if spawnrate>2:
+					if rdm.random()>(0.75-(2/spawnrate)+(player.hp/400)):
+						spawnrate-=1
+			else:
+				slot.cooldown -= 1
+	
+	num=int(rdm.random()*spawnrate)
+	if num<2:
 		if num: position=(0.36, -0.08)
 		else: position=(-0.08, 0.12)
 		new_enemy=Enemy(position, (200, 0, 0))
@@ -60,6 +148,10 @@ while not done:
 		dead = manny.move()
 		if dead:
 			enemies.remove(manny)
+			player.hp-=1
+			health, gold = player.display(my_font)
+			if not player.hp:
+				inProgress=False
 		manny.display(screen, x, y, w, h, enemy_size)
 		
 	for slot in all_slots:
